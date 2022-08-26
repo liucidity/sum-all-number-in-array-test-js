9c1c3aac5eb3eb475cde15ca5f5b3e28e35f7a55
Travis <travisliu708@gmail.com>
2022-08-25 19:19:18 -0700

    added function to sum arrays recursively

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      return total += sumItems(element);
+    } else {
+      total += element;
+    }
+  });
+  return total;
+  //base case: single layer array and can add sum
 }
 
 module.exports = sumItems;
